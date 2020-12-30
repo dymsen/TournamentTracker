b37dac8570a29b6b8931211d795f5adaadd1e978
git.dymsen@pm.me <git.dymsen@pm.me>
2020-12-30 15:53:43 +0100

    Adding TrackerLibary

---
+++ TrackerLibrary/TrackerLibrary/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public class PersonModel
+    {
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        public string EmailAdress { get; set; }
+        public string CellphoneNumber { get; set; }
+    }
+}
